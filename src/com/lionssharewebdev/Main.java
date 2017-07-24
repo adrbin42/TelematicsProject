39fa92d83ba676630ee9185a19fe8abf6cdb559d
adrienne <adriennebing@gmail.com>
2017-07-23 20:01:31 -0400

    Java to JSON complete

--- src/com/lionssharewebdev/Main.java
+++ src/com/lionssharewebdev/Main.java
@@ -1,33 +1,47 @@
 package com.lionssharewebdev;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        VehicleInfo newVehicle = new VehicleInfo();
-        Scanner scanner = new Scanner(System.in);
+//        VehicleInfo newVehicle = new VehicleInfo();
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Please enter VIN : ");
+//        String vinInput = scanner.nextLine();
+//        newVehicle.setVIN(vinInput);
+//
+//        System.out.print("Please enter the miles traveled from your odometer : ");
+//        double odometerInput = scanner.nextDouble();
+//        newVehicle.setOdometer(odometerInput);
+//
+//        System.out.print("Please enter the gas used : ");
+//        double gasInput = scanner.nextDouble();
+//        newVehicle.setConsumption(gasInput);
+//
+//        System.out.print("Please enter odometer reading for your last oil change : ");
+//        double oilChangeInput = scanner.nextDouble();
+//        newVehicle.setOdometerLastOil(oilChangeInput);
+//
+//        System.out.println("Please enter the engine size : ");
+//        double engineSizeInput = scanner.nextDouble();
+//        newVehicle.setEngineSize(engineSizeInput);
+//
+//        //write vehicle report to json file
+//        try {
+//            TelematicsService.report(newVehicle);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        System.out.println("-----------------------------------------------------------------------");
+        try {
+            TelematicsService.convertJSONFileToObj();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        System.out.print("Please enter VIN : ");
-        String vinInput = scanner.nextLine();
-        newVehicle.setVIN(vinInput);
 
-        System.out.print("Please enter the miles traveled from your odometer : ");
-        double odometerInput = scanner.nextDouble();
-        newVehicle.setOdometer(odometerInput);
-
-        System.out.print("Please enter the gas used : ");
-        double gasInput = scanner.nextDouble();
-        newVehicle.setConsumption(gasInput);
-
-        System.out.print("Please enter odometer reading for your last oil change : ");
-        double oilChangeInput = scanner.nextDouble();
-        newVehicle.setOdometerLastOil(oilChangeInput);
-
-        System.out.print("Please enter the engine size : ");
-        double engineSizeInput = scanner.nextDouble();
-        newVehicle.setEngineSize(engineSizeInput);
-
-        File
     }
 }
--- src/com/lionssharewebdev/TelematicsService.java
+++ src/com/lionssharewebdev/TelematicsService.java
@@ -1,15 +1,47 @@
 package com.lionssharewebdev;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.core.JsonParseException;
+
+
+
+
 
 /**
  * Created by adrienne on 7/21/17.
  */
+
 public class TelematicsService {
-    public static void report(VehicleInfo vehicleInfo){
+    public static void report(VehicleInfo newVehicle) throws JsonGenerationException, JsonMappingException, IOException{
         ObjectMapper mapper = new ObjectMapper();
-        String json = mapper.writeValueAsString(vehicleInfo);
+        String vinNumber = newVehicle.getVIN();
+        mapper.writeValue(new File(vinNumber + ".json"), newVehicle);
+        String jsonString = mapper.writeValueAsString(newVehicle);
+        System.out.print("Your vehicle information is as follows: "+ jsonString);
+    }
+
+    public static void convertJSONFileToObj() throws IOException {
+        int incrementVIObject = 0;
+        File file = new File(".");
+        for (File f : file.listFiles()) {
+            if (f.getName().endsWith(".json")) {
+                incrementVIObject++;
+                Scanner scannerFile = new Scanner(f.getName());
+                ObjectMapper mapper = new ObjectMapper();
+                String jsonString = scannerFile.toString();
+                VehicleInfo vehicleInfoObject = mapper.readValue(jsonString, VehicleInfo.class);
+                System.out.println("The java object name is: " + vehicleInfoObject + incrementVIObject );
+                System.out.println("The object information read from file is : " + vehicleInfoObject);
+            }
+        }
+
+
+
     }
 }
